00426ad8853343c444dc38c1a6e24c85f4f7b2da
Rex <161436089+rexcgods@users.noreply.github.com>
2024-04-03 23:50:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const { exec } = require('child_process');
 
 // Ganti dengan token bot Telegram Anda
-const token = 'Done! Congratulations on your new bot. You will find it at t.me/erdisonbot. You can now add a description, about section and profile picture for your bot, see /help for a list of commands. By the way, when you've finished creating your cool bot, ping our Bot Support if you want a better username for it. Just make sure the bot is fully operational before you do this.
-
-Use this token to access the HTTP API:
-7069670027:AAGrM4SX8wAyoFmZpKQQo83qS3aANewmleI
-Keep your token secure and store it safely, it can be used by anyone to control your bot.
-
-For a description of the Bot API, see this page: https://core.telegram.org/bots/api';
+const token = '7069670027:AAGrM4SX8wAyoFmZpKQQo83qS3aANewmleI';
 
 // Inisialisasi bot dengan token
 const bot = new TelegramBot(token, { polling: true });
